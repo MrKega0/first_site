50d8798f239d8ad6da538719272bb0486baa1fea
danila <danilalapin707@gmail.com>
2025-04-13 17:27:59 +0300

    initial commit

---
+++ mainapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    request.session['a'] = 'a'
+    return render(request, "mainapp/index.html")
+
+def about_me(request):
+    return render(request, "mainapp/about_me.html")
+
+def sp(request):
+    print(request.session['a'])
+    name = request.GET.get('name')
+    last_name = request.GET.get('last_name')
+    print(name)
+    if not name:
+        name = 'Безымянный лох'
+    context = {'name':name, 'last_name':last_name }
+    return render(request, "mainapp/sp.html", context)
