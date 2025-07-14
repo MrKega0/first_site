e0ffbcbcc46ddb55e139434bcacaa741f5d53361
MrKega0 <danilalapin707@gmail.com>
2025-07-14 17:00:15 +0300

    CBV вьюшки
    Co-authored-by: Romanenko Vladimir <romanenko-vova@users.noreply.github.com>

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-from mainapp.views import index, about_me, game_info, add_comment, toggle_favorite, recommended_games, asynh_get_recommended_games
+from mainapp.views import IndexView, about_me,GameInfoView, add_comment, toggle_favorite, recommended_games, asynh_get_recommended_games
 from django.conf import settings
 from django.conf.urls.static import static
 
 app_name = "mainapp"
 
 urlpatterns = [
-    path('', index),
+    path('', IndexView.as_view()),
     path('about-me/', about_me),
     # path('sp/', sp),
-    path('game/<int:game_id>', game_info),
+    path('game/<int:game_id>', GameInfoView.as_view()),
     path('add_comments/',add_comment),
     path('toggle_favorite/',toggle_favorite),
     path('recommended_games',recommended_games),
